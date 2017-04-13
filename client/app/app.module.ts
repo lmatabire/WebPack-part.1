64e703f3aa4adb2c3da5e717ce5bd9081ee3bf20
lmatabire <Matabire23@gmail.com>
2017-04-13 16:25:18 -0700

    Bootstrap on Webpack

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,13 +2,14 @@ import * as angular from 'angular';
 
   // NPM Angular dependencies
 import 'angular-ui-router';
-
+import 'angular-ui-bootstrap';
 // High Level Pages
 import emojiList from './emojiList/emojiList.module';
  // Config stuff
 import {Config} from './app.config';
 const dependencies = [
   'ui.router',
+  'ui.bootstrap',
   emojiList
 ];
 angular.module('app', dependencies)
